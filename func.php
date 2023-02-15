789d4eb90ab2239809a4740e6ac9a5cfd061512a
jsprw88 <macbook@macbooks-mbp.home>
2023-02-15 19:21:06 +0000

    add func.php

---
+++ func.php
@@ -0,0 +1,11 @@
+<?php function reverses(int $num): int
+{
+
+    $rev = strrev((string) $num);
+
+    if ($rev[-1] === -) {
+        $curr = substr($rev, 0, (strlen($rev) - 1));
+        $rev = - . $curr;
+    }
+    return (int) $rev;
+}
